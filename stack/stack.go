e52f406e32ab42279c9db010731f855a7354e781
Alonso Bonilla <luis.labp06@gmail.com>
2024-09-01 01:50:49 -0500

    public struct and add empty function

--- stack/stack.go
+++ stack/stack.go
@@ -1,32 +1,36 @@
 package stack
 
-type stack[T any] struct {
+type Stack[T any] struct {
 	topOfStack int
 	arr        []T
 }
 
-func NewStack[T any](capacity int) *stack[T] {
-	return &stack[T]{
+func NewStack[T any](capacity int) *Stack[T] {
+	return &Stack[T]{
 		topOfStack: -1,
 		arr:        make([]T, capacity),
 	}
 }
 
-func (s *stack[T]) Pop() T {
+func (s *Stack[T]) Pop() T {
 	element := s.arr[s.topOfStack]
 	s.topOfStack--
 	return element
 }
 
-func (s *stack[T]) Top() (T, bool) {
+func (s *Stack[T]) Top() T {
 	if s.topOfStack < 0 {
 		var zeroValue T
-		return zeroValue, false
+		return zeroValue
 	}
-	return s.arr[s.topOfStack], true
+	return s.arr[s.topOfStack]
 }
 
-func (s *stack[T]) Push(element T) {
+func (s *Stack[T]) Push(element T) {
 	s.topOfStack++
 	s.arr[s.topOfStack] = element
+}
+
+func (s Stack[T]) Empty() bool {
+	return s.topOfStack < 0
 }

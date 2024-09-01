8392e2272c3fa4ad6948df51602f9f199f66a0ba
Alonso Bonilla <luis.labp06@gmail.com>
2024-09-01 01:51:30 -0500

    infix to postfix

--- cmd/infix-postfix/main.go
+++ cmd/infix-postfix/main.go
@@ -2,40 +2,103 @@ package main
 
 import (
 	stack "exercises/stack"
+	"fmt"
+	"math"
+	"strconv"
 )
 
-var signs = map[rune]int{
+var symbolsWeight = map[rune]int{
 	'+': 0,
 	'-': 0,
 	'*': 1,
 	'/': 1,
 	'^': 2,
-	'(': 10,
-	')': 11,
+	'(': 3,
+	')': 3,
+}
+
+type operation func(float64, float64) float64
+
+var symbolsOperation = map[rune]operation{
+	'+': func(a, b float64) float64 { return a + b },
+	'-': func(a, b float64) float64 { return a - b },
+	'*': func(a, b float64) float64 { return a * b },
+	'/': func(a, b float64) float64 { return a / b },
+	'^': func(a, b float64) float64 { return math.Pow(a, b) },
 }
 
 func main() {
-	input := "a+b*c+(2^(2^3))*g"
-	signsStack := stack.NewStack[rune](len(input))
-	output := make([]rune, len(input))
+	input := "6*((5+(2+3^2)*8)+3)"
 
-	for _, element := range input {
+	output := []rune{}
+	stack := stack.NewStack[rune](len(input))
+	for _, char := range input {
 
-		if valuePrecedence, ok := signs[element]; ok {
-
-			top, ok := signsStack.Top()
-			if ok {
-				if signs[top] > valuePrecedence {
-					signsStack.Push(element)
-				} else {
-					
-				}
-			} else {
-				signsStack.Push(element)
-			}
-		} else {
-			output = append(output, element)
+		// If the character not is a symbol
+		if _, ok := symbolsWeight[char]; !ok {
+			output = append(output, char)
+			continue
 		}
 
+		//If the character is ')', we extract all the elements up to the first parenthesis
+		if char == ')' {
+			bracketsExtract(stack, &output)
+			continue
+		}
+
+		e := stack.Top()
+		// Add the symbol if it has a greater weight than the top symbol of the stack or if the stack is empty
+		if stack.Empty() || symbolsWeight[char] > symbolsWeight[e] {
+			stack.Push(char)
+		} else {
+			/* Add the symbol to the output (postfix expression) while making sure that the top symbol is greater than or equal to the input character and different from '(' because it can't be  add the '(' symbol to the postfix expression */
+
+			for !stack.Empty() && e != '(' && symbolsWeight[char] <= symbolsWeight[e] {
+				stack.Pop()
+				output = append(output, e)
+				e = stack.Top()
+			}
+
+			//Push the input character to the stack
+			stack.Push(char)
+		}
 	}
+
+	//Add all remaining symbols to the output (postfix expression)
+	for !stack.Empty() {
+		output = append(output, stack.Pop())
+	}
+	//Print the postfix expression
+	fmt.Printf("%c\n", output)
+
+	//Compute the result of the postifx expression
+	fmt.Println(computePostFixExpression(output))
+}
+
+// Extract all the symbols that are between the brackets
+func bracketsExtract(stack *stack.Stack[rune], output *[]rune) {
+	stackElement := stack.Pop()
+	for stackElement != '(' {
+		*output = append(*output, stackElement)
+		stackElement = stack.Pop()
+	}
+}
+
+// Compute the postifix expression
+func computePostFixExpression(postFix []rune) float64 {
+	result := stack.NewStack[float64](len(postFix))
+
+	for _, char := range postFix {
+		if _, ok := symbolsOperation[char]; ok {
+			num1 := result.Pop()
+			num2 := result.Pop()
+
+			result.Push(symbolsOperation[char](num2, num1))
+		} else {
+			value, _ := strconv.ParseFloat(string(char), 64)
+			result.Push(value)
+		}
+	}
+
+	return result.Top()
 }
